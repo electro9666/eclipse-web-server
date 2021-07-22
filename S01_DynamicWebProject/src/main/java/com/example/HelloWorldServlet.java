b438ec3c3a98dd9d40ffe0fdbaadd952cb9621db
electro9666 <electro9666@gmail.com>
2021-07-22 18:24:37 +0900

    serve modules without publishing

--- S01_DynamicWebProject/src/main/java/com/example/HelloWorldServlet.java
+++ S01_DynamicWebProject/src/main/java/com/example/HelloWorldServlet.java
@@ -38,6 +38,7 @@ public class HelloWorldServlet implements Servlet {
 	@Override
 	public void service(ServletRequest req, ServletResponse res) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		System.out.println("service3");
 		
 	}
 
